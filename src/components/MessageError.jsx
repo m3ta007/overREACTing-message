a472b436d53b111d3a99a5be3f5f165534c49533
Anna Kova <m3ta007@gmail.com>
2020-07-22 20:01:21 -0400

    Dependecy update

--- src/components/MessageError.jsx
+++ src/components/MessageError.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Message from '../components/Message'
 
+// Object.assign:
 function MessageError(props) {
-  return <Message type="error" text={props.text} />
+  const base = {
+    type: 'error',
+    icon: 'notification_important',
+    iconText: 'Error',
+  }
+
+  const newProps = Object.assign({}, props, base)
+  // const newProps = { ...props, ...base }
+
+  return <Message {...newProps} />
 }
+
+//// Spread-operator:
+// function MessageError(props) {
+//   return <Message {...props} type="error" />
+// }
 
 MessageError.propTypes = {
   props: PropTypes.objectOf(PropTypes.string),
